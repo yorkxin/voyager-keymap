924fa2d5e9c8e718c3b207ff7fb54e5458286110
Yu-Cheng Chuang <ducksteven@gmail.com>
2026-07-20 14:27:51 +0900

    devcontainer: use zsa firmware

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -14,7 +14,7 @@ userspacePath="$1"
 git config --global --add safe.directory "$userspacePath"
 git submodule update --init --recursive
 
-[ -d /workspaces/qmk_firmware ] || git clone https://github.com/qmk/qmk_firmware.git /workspaces/qmk_firmware
+[ -d /workspaces/qmk_firmware ] || git clone -b firmware25 https://github.com/zsa/qmk_firmware.git /workspaces/qmk_firmware
 git config --global --add safe.directory /workspaces/qmk_firmware
 
 qmk config user.qmk_home=/workspaces/qmk_firmware
